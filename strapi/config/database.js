c967ded294d9dc9c3470988623bd546cb407b3ea
khanh <khanh.trieu@riviu.vn>
2022-07-27 17:29:57 +0700

    ip mysql

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: '172.17.0.2',
+      host: '172.17.0.4',
       // host: '103.116.104.166',
       port: 3306,
       database: 'strapi',
